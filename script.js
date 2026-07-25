a256ec42954d5dacc940cf9852f92ea1b9e479f5
hayfr <hayokay3@gmail.com>
2026-07-24 18:09:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,24 +2,36 @@ const pageSound=new Audio("https://assets.mixkit.co/active_storage/sfx/2568/2568
 pageSound.volume=.15;
 
 document.addEventListener("DOMContentLoaded",()=>{
-    document.body.classList.add("page-enter");
+    const cover=document.getElementById("pageCover");
     const currentPage=window.location.pathname.split("/").pop()||"index.html";
+
+    requestAnimationFrame(()=>{
+        cover.classList.add("hide");
+    });
+
     document.querySelectorAll("nav a").forEach(link=>{
         const nextPage=link.getAttribute("href");
+
         if(nextPage===currentPage)link.classList.add("active");
+
         link.addEventListener("click",e=>{
             if(nextPage===currentPage)return;
+
             e.preventDefault();
             pageSound.currentTime=0;
             pageSound.play().catch(()=>{});
-            document.body.classList.add("page-exit");
+            cover.classList.remove("hide");
+
             setTimeout(()=>{
                 window.location.href=nextPage;
-            },150);
+            },100);
         });
     });
 });
 
 window.addEventListener("pageshow",()=>{
-    document.body.classList.remove("page-exit");
+    const cover=document.getElementById("pageCover");
+    requestAnimationFrame(()=>{
+        cover.classList.add("hide");
+    });
 });
